e0a6d610c01a7ceb76de541a96bbb9acfd354c39
crapanza1u <crapanza1u@tncy-1-18-62>
2016-03-07 17:37:11 +0100

    Plus que deux ! :)

--- projet/src/ants/StunThrowerAnt.java
+++ projet/src/ants/StunThrowerAnt.java
@@ -4,7 +4,9 @@ import core.Bee;
 
 
 public class StunThrowerAnt extends ThrowerAnt {
-
+	
+	private int time=0;
+	
 	public StunThrowerAnt() {
 		super();
 		foodCost=6;
@@ -15,7 +17,10 @@ public class StunThrowerAnt extends ThrowerAnt {
 		if (this.armor>0){//la throwerAnt ne fait une action que si son armor est superieur a 0
 			Bee target = getTarget();
 			if (target != null) {
-
+				for (int time=0;time<2;time++) {
+				
+					
+				}
 			}
 		}
 		
--- projet/src/core/Bee.java
+++ projet/src/core/Bee.java
@@ -64,6 +64,8 @@ public class Bee extends Insect {
 		
 	}
 	
+	
+	
 	//je cree un getteur pour le DAMAGE
 	public int getDamage(){
 		return Bee.DAMAGE;
