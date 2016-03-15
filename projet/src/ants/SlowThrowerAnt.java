db754ec9606ee8a2f01cf34e297bd6ae23af46e1
cedric <Cedric@10.10.104.158>
2016-03-15 21:17:31 +0100

    :) :)

---
+++ projet/src/ants/SlowThrowerAnt.java
@@ -0,0 +1,31 @@
+package ants;
+
+import java.util.ArrayList;
+
+import core.AntColony;
+import core.Bee;
+
+public class SlowThrowerAnt extends ThrowerAnt {
+	
+	ArrayList<Bee> abeillesSlow=new ArrayList<Bee>();
+	
+	public SlowThrowerAnt(){
+		super();
+		foodCost=4;
+		damage=0;
+		name="SlowT";
+	}
+	
+	public Bee getTarget () {
+		return place.getClosestBee(0, 2);
+	}
+	
+	public void action(AntColony colony){
+		
+		Bee target=getTarget();
+		if (target != null){
+			target.setSlowStun(true);
+		}
+		
+	}
+}
