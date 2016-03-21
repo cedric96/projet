34c8aaa8b2c32b4d1a7cad413b16a030e1a89667
mea3u <mea3u@tncy-1-18-05>
2016-03-21 13:22:53 +0100

    aze

---
+++ projet/src/ants/myAnt.java
@@ -0,0 +1,9 @@
+package ants;
+
+public class myAnt extends ThrowerAnt{
+	
+	public myAnt(){
+		super();
+	}
+
+}
---
+++ projet/src/core/GlassCanon.java
@@ -0,0 +1,11 @@
+package core;
+
+public class GlassCanon extends Bee {
+
+	public GlassCanon(int armor) {
+		super(1);
+		this.setDamage(4);
+		// TODO Auto-generated constructor stub
+	}
+
+}
